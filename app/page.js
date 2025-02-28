ef3b9fbb683a26655cd8c9406dc4ff7c9052d686
prasangeet <b23ch1033@iitj.ac.in>
2025-02-28 16:47:11 +0530

    Initialization of the project

--- app/page.js
+++ app/page.js
@@ -1,9 +1,9 @@
-import Hero from "@/components/hero";
-import About from "@/components/about";
-import Skills from "@/components/skills";
-import Projects from "@/components/projects";
-import Contact from "@/components/contact";
-import CursorFollower from "@/components/cursor-follower";
+import Hero from "../components/hero";
+import About from "../components/about";
+import Skills from "../components/skills";
+import Projects from "../components/projects";
+import Contact from "../components/contact";
+import CursorFollower from "../components/cursor-follower";
 
 export default function Home() {
   return (
